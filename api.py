2a227410b057d6045bc6efb3e35f5850d463a3a9
gomzalo <201318652@ingenieria.usac.edu.gt>
2018-09-14 15:45:12 -0600

    Dependencia de googleapiclient corregida

--- api.py
+++ api.py
@@ -1,5 +1,7 @@
 # Send to single device.
 from __future__ import print_function
+
+#from googleapiclient import discovery
 from googleapiclient.discovery import build
 from httplib2 import Http
 from oauth2client import file, client, tools
@@ -30,7 +32,7 @@ def fcmService(tipo):
     elif tipo == "0":
         tipoHuella = "incorrecta, no se abrio la puerta"
     elif tipo == "2":
-        tipoHuella = "incorrecta 3 veces, se bloqueo el sistema"
+        tipoHuella = "incorrecta por tercera vez, se ha bloqueado el ingreso"
 
 
     topic_name = "Arqui1"
@@ -48,13 +50,34 @@ def fcmService(tipo):
 # If modifying these scopes, delete the file token.json.
 SCOPES = 'https://www.googleapis.com/auth/gmail.readonly'
 
+def gmailTest():
+    """Shows basic usage of the Gmail API.
+    Lists the user's Gmail labels.
+    """
+    store = file.Storage('token.json')
+    creds = store.get()
+    if not creds or creds.invalid:
+        flow = client.flow_from_clientsecrets('credentials.json', SCOPES)
+        creds = tools.run_flow(flow, store)
+    service = build('gmail', 'v1', http=creds.authorize(Http()))
 
+    # Call the Gmail API
+    results = service.users().labels().list(userId='me').execute()
+    labels = results.get('labels', [])
+
+    if not labels:
+        print('No labels found.')
+    else:
+        print('Labels:')
+        for label in labels:
+            print(label['name'])
 
 
 @app.route("/<num>")
 def setNum(num):
     num = num
     fcmService(num)
+    gmailTest()
     return str(payload)
 
 if __name__ == '__main__':
